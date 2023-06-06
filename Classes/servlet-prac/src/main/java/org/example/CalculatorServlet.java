08271a82229a17edcc5f135c691dc4042d2bb90c
jwywoo <jwywoo26@gmail.com>
2023-06-06 19:40:18 +0900

    Servlet Practice: GenericServlet implementation done(abstractions are extends)

--- Classes/servlet-prac/src/main/java/org/example/CalculatorServlet.java
+++ Classes/servlet-prac/src/main/java/org/example/CalculatorServlet.java
@@ -11,19 +11,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/calculate")
-public class CalculatorServlet implements Servlet {
+public class CalculatorServlet extends GenericServlet {
     private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
     private ServletConfig servletConfig;
-    @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
-        log.info("init");
-        this.servletConfig = servletConfig;
-    }
-
-    @Override
-    public ServletConfig getServletConfig() {
-        return servletConfig;
-    }
 
     @Override
     public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
@@ -38,13 +28,4 @@ public class CalculatorServlet implements Servlet {
         writer.println(result);
     }
 
-    @Override
-    public String getServletInfo() {
-        return null;
-    }
-
-    @Override
-    public void destroy() {
-        // Resource release
-    }
 }
