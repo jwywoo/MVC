696181c61d0fcec121d0fd415ca56916c7deff7f
jwywoo <jwywoo26@gmail.com>
2023-06-06 19:36:25 +0900

    Servlet Practice: Calculator functions copied and Servlet implementation done

--- Classes/servlet-prac/src/main/java/org/example/CalculatorServlet.java
+++ Classes/servlet-prac/src/main/java/org/example/CalculatorServlet.java
@@ -1,29 +1,41 @@
 package org.example;
 
+import org.example.calculator.domain.Calculator;
+import org.example.calculator.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/calculate")
 public class CalculatorServlet implements Servlet {
     private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
-
+    private ServletConfig servletConfig;
     @Override
-    public void init(ServletConfig config) throws ServletException {
+    public void init(ServletConfig servletConfig) throws ServletException {
         log.info("init");
+        this.servletConfig = servletConfig;
     }
 
     @Override
     public ServletConfig getServletConfig() {
-        return null;
+        return servletConfig;
     }
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        log.info("Service");
+        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+        String operator = request.getParameter("operator");
+        int operand2 = Integer.parseInt(request.getParameter("operand2"));
 
+        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        PrintWriter writer = response.getWriter();
+        writer.println(result);
     }
 
     @Override
@@ -33,6 +45,6 @@ public class CalculatorServlet implements Servlet {
 
     @Override
     public void destroy() {
-
+        // Resource release
     }
 }
--- Classes/servlet-prac/src/main/java/org/example/WebApplicationServerLauncher.java
+++ Classes/servlet-prac/src/main/java/org/example/WebApplicationServerLauncher.java
@@ -4,10 +4,12 @@ import org.apache.catalina.startup.Tomcat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.ServletConfig;
 import java.io.File;
 
 public class WebApplicationServerLauncher {
     private static final Logger logger = LoggerFactory.getLogger(WebApplicationServerLauncher.class);
+
     public static void main(String[] args) throws Exception {
         String webappDirLocation = "webapp/";
         Tomcat tomcat = new Tomcat();
