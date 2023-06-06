6fbbdf9be9c79c23a8bd28f52debfae55f7df38a
jwywoo <jwywoo26@gmail.com>
2023-06-06 15:34:26 +0900

    WAS Practice: Step1 creating a request and parsing the request as given

--- Classes/was-prac/src/test/java/org/example/RequestLineTest.java
+++ Classes/was-prac/src/test/java/org/example/RequestLineTest.java
@@ -9,6 +9,7 @@ public class RequestLineTest {
     void createRequestTest() {
         RequestLine requestLine = new RequestLine("GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1");
 
-        assertThat()
+        assertThat(requestLine).isNotNull();
+        assertThat(requestLine).isEqualTo(new RequestLine("GET", "/calculate", "operand1=11&operator=*&operand2=55"));
     }
 }
