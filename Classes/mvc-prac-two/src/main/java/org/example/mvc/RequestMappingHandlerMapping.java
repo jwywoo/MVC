bc2658831015143500270c11852a8404481906ce
jwywoo <jwywoo26@gmail.com>
2023-06-11 16:38:25 +0900

    Custom MVC Framework(Spring like): Form using Forward class and related RequestMapping done

--- Classes/mvc-prac-two/src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
+++ Classes/mvc-prac-two/src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
@@ -11,8 +11,9 @@ public class RequestMappingHandlerMapping {
 
     void init() {
         mappings.put(new HandlerKey(RequestMethod.GET,"/"), new HomeController());
-        mappings.put(new HandlerKey(RequestMethod.GET,"users"), new UserListController());
-        mappings.put(new HandlerKey(RequestMethod.POST,"users"), new UserCreateController());
+        mappings.put(new HandlerKey(RequestMethod.GET,"/users"), new UserListController());
+        mappings.put(new HandlerKey(RequestMethod.POST,"/users"), new UserCreateController());
+        mappings.put(new HandlerKey(RequestMethod.GET, "/users/forms"), new ForwardController("/users/fowrad"))
     }
     public Controller findHandler(String urlPath) {
         return mappings.get(urlPath);
