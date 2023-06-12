f66f0212a902b247d6c302ca5326ef96edab3d13
jwywoo <jwywoo26@gmail.com>
2023-06-12 15:47:12 +0900

    DI(Dependency Injection): Beanfactory test and Beanfactory class implemented

--- Classes/di-prac/src/main/java/org/example/di/BeanFactory.java
+++ Classes/di-prac/src/main/java/org/example/di/BeanFactory.java
@@ -1,10 +1,15 @@
 package org.example.di;
 
+import org.example.annotation.Controller;
+import org.example.annotation.Inject;
 import org.example.controller.UserController;
+import org.reflections.ReflectionUtils;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+import static org.example.di.BeanFactoryUtils.getInjectedConstructor;
 
 public class BeanFactory {
     private final Set<Class<?>> preInstantiatedClazz;
@@ -12,6 +17,53 @@ public class BeanFactory {
 
     public BeanFactory(Set<Class<?>> preInstantiatedClazz) {
         this.preInstantiatedClazz = preInstantiatedClazz;
+        initialize();
+    }
+
+    private void initialize() {
+        for (Class<?> clazz : preInstantiatedClazz) {
+            Object instance = createInstance(clazz);
+            beans.put(clazz, instance);
+        }
+    }
+
+    private Object createInstance(Class<?> clazz) {
+        // Constructor
+        Constructor<?> constructor = findConstructor(clazz);
+
+        // Parameters
+        List<Object> parameters = new ArrayList<>();
+        for (Class<?> typeClass : constructor.getParameterTypes()) {
+            // getting instance of typeClass and put it inside of parameters
+            parameters.add(getParameterByClass(typeClass));
+        }
+
+        // Creating instance
+        try {
+            return constructor.newInstance(parameters.toArray());
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Constructor<?> findConstructor(Class<?> clazz) {
+        // Bring all constructors with withAnnotation Inject
+        Constructor<?> constructor = getInjectedConstructor(clazz);
+
+        if (Objects.nonNull(constructor)) {
+            return constructor;
+        }
+        return clazz.getConstructors()[0];
+    }
+
+    private Object getParameterByClass(Class<?> typeClass) {
+        Object instanceBean = getBean(typeClass);
+
+        if (Objects.nonNull(instanceBean)) {
+            return instanceBean;
+        }
+
+        return createInstance(typeClass);
     }
 
     public <T> T getBean(Class<T> requestedType) {
