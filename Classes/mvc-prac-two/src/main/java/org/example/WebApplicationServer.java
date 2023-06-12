2d7e5e1ed222101d5bc5ba3f5eb4972a0c87b92f
jwywoo <jwywoo26@gmail.com>
2023-06-12 14:38:14 +0900

    DI(Dependency Injection): Dependencies added

--- Classes/mvc-prac-two/src/main/java/org/example/WebApplicationServer.java
+++ Classes/mvc-prac-two/src/main/java/org/example/WebApplicationServer.java
@@ -12,7 +12,7 @@ public class WebApplicationServer {
     public static void main(String[] args) throws Exception {
         String webappDirLocation = "webapps/";
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8000);
+        tomcat.setPort(8080);
 
         tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
         log.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
